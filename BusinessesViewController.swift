489429285b539f4eb54b84138b0faa964932b52a
Jacob Mittelstaedt <jacobmittelstaedt@Jacobs-MacBook-Pro.local>
2017-02-19 23:33:11 -0500

    added search function

--- BusinessesViewController.swift
+++ BusinessesViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UIScrollViewDelegate {
     
+    var isMoreDataLoading = false
     var businesses: [Business]!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
@@ -27,6 +28,7 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
             
             self.businesses = businesses
             self.filteredData = businesses
+            self.isMoreDataLoading = false
             self.tableView.reloadData()
             if let businesses = businesses {
                 for business in businesses {
@@ -68,9 +70,35 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         return cell
     }
     
+    func loadMoreData() {
+        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+            self.businesses = businesses
+            self.filteredData = businesses
+            self.isMoreDataLoading = false
+            self.tableView.reloadData()
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
+            }
+            
+        })
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (!isMoreDataLoading) {
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
+                isMoreDataLoading = true
+                loadMoreData()
+            }
+        }
+    }
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        
-        
         
         filteredData = searchText.isEmpty ? businesses : businesses?.filter({(business: Business) -> Bool in
             
